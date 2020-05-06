8faefa0b1fa51039403d6c39525713eb2f0d0a18
Anusuriya Devaraju <anusuriya_asd@yahoo.com>
2020-05-06 19:47:30 +0200

    renamed app.py to _main_.py (so that can be as a standalone application -> python -m package_name) updated requirements.txt (version information)

--- fuji_server/__main__.py
+++ fuji_server/__main__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
 import configparser as ConfigParser
 import os
 from logging.config import fileConfig
--- fuji_server/controllers/__init__.py
+++ fuji_server/controllers/__init__.py
@@ -10,7 +10,7 @@ class BaseTestCase(TestCase):
 
     def create_app(self):
         logging.getLogger('connexion.operation').setLevel('ERROR')
-        app = connexion.App(__name__, specification_dir='../swagger/')
+        app = connexion.App(__name__, specification_dir='../yaml/')
         app.app.json_encoder = JSONEncoder
         app.add_api('swagger.yaml')
         return app.app
