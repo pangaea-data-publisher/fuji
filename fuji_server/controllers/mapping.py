6c8d74ca3d19e042d44f991c49921b12ad325d34
huberrob <huberrob@users.noreply.github.com>
2020-05-11 14:59:22 +0200

    Replaced 'data_identfier' with 'object_identifier' and 'data_access_url' with 'object_content_identifier'

--- fuji_server/controllers/mapping.py
+++ fuji_server/controllers/mapping.py
@@ -14,8 +14,8 @@ class Sources(enum.Enum):
     SIGN_POSTING = 'Signposting Typed Links'
 
 # reference metadata elements
-CORE_METADATA = ['data_identifier', 'creator', 'title', 'publisher', 'publication_date', 'summary', 'keywords',
-                 'data_access_url', 'access_level', 'embargoe', 'access_info', 'related_resources', 'provenance',
+CORE_METADATA = ['object_identifier', 'creator', 'title', 'publisher', 'publication_date', 'summary', 'keywords',
+                 'object_content_identifier', 'access_level', 'embargoe', 'access_info', 'related_resources', 'provenance',
                  'license','data_file_format', 'type', 'data_size']
 
 # core metadata elements (FsF-F2-01M)
@@ -25,13 +25,13 @@ REQUIRED_CORE_METADATA = ['creator', 'title', 'publisher', 'publication_date', '
 
 # https://www.dublincore.org/specifications/dublin-core/dcmi-terms/
 # license: Recommended practice is to identify the license document with a URI. If this is not possible or feasible, a literal value that identifies the license may be provided.
-DC_MAPPING = {'data_identifier': 'identifier', 'creator': 'creator', 'title': 'title',
+DC_MAPPING = {'object_identifier': 'identifier', 'creator': 'creator', 'title': 'title',
               'publisher': 'publisher', 'publication_date': 'date', 'summary': 'abstract',
               'keywords': 'subject', 'type': 'type',
               'license': 'license', 'data_file_format': 'format'}
 
 # https://ogp.me/
-OG_MAPPING = {'title': 'og:title', 'data_identifier': 'og:url', 'summary': 'og:description',
+OG_MAPPING = {'title': 'og:title', 'object_identifier': 'og:url', 'summary': 'og:description',
               'type': 'og:type', 'publisher': 'og:site_name'}
 
 # Schema.org
@@ -42,9 +42,9 @@ SCHEMAORG_MAPPING = '{title: name, type: "@type", date: datePublished."@value" |
                            'creator_last: creator[*].familyName || author[*].familyName || creator.familyName || author.familyName,' \
                            'publisher: publisher.name, license: (license."@id" || license.license."@id") || license, ' \
                            'summary: description, keywords: keywords, publication_date: datePublished, data_file_format: encodingFormat,' \
-                           'data_identifier: ("@id" || url."@id" || identifier.value ) || url, data_access_url: (distribution.contentUrl || distribution[*].contentUrl)}'
+                           'object_identifier: ("@id" || url."@id" || identifier.value ) || url, object_content_identifier: (distribution.contentUrl || distribution[*].contentUrl)}'
 
-DATACITE_JSON_MAPPING = '{ data_identifier: id, type: types.resourceTypeGeneral,  ' \
+DATACITE_JSON_MAPPING = '{ object_identifier: id, type: types.resourceTypeGeneral,  ' \
                         'creator: creators[*].name, creator_first: creators[*].givenName,' \
                         'creator_last: creators[*].familyName, publisher: publisher, ' \
                         'title: titles[0].title, keywords: subjects[*].subject, publication_date: dates[?dateType ==\'Available\'].date,' \
