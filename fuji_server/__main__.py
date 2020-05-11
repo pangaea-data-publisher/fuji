48c5a83b0b54942bffb99d26c4c8f49d8ca2ec8f
Anusuriya Devaraju <anusuriya_asd@yahoo.com>
2020-05-11 12:02:00 +0200

    ensured log dir is created if it not exists modified metric name FsF-I3-01M added re3data and datacite endpoint in config file (server.ini)

--- fuji_server/__main__.py
+++ fuji_server/__main__.py
@@ -4,9 +4,7 @@ import configparser as ConfigParser
 import logging
 import os
 from logging.config import fileConfig
-
 import connexion
-
 from fuji_server import encoder
 from fuji_server.controllers.preprocessor import Preprocessor
 
@@ -27,17 +25,21 @@ def main():
     METRIC_YAML = config['SERVICE']['metrics_yaml']
     METRIC_YML_PATH = os.path.join(ROOT_DIR, YAML_DIR,METRIC_YAML)
     SPDX_URL = config['EXTERNAL']['spdx_license_github']
+    DATACITE_API_REPO = config['EXTERNAL']['datacite_api_repo']
+    RE3DATA_API = config['EXTERNAL']['re3data_api']
     preproc = Preprocessor()
     preproc.retrieve_metrics_yaml(METRIC_YML_PATH)
     preproc.retrieve_licenses(SPDX_URL)
+    preproc.retrieve_datacite_re3repos(RE3DATA_API, DATACITE_API_REPO)
     logger.info('Total metrics defined: {}'.format(preproc.get_total_metrics()))
     logger.info('Total SPDX licenses : {}'.format(preproc.get_total_licenses()))
+    logger.info('Total re3repositories found from datacite api : {}'.format(len(preproc.get_re3repositories())))
 
     app = connexion.FlaskApp(__name__, specification_dir=YAML_DIR)
     API_YAML = os.path.join(ROOT_DIR, YAML_DIR, config['SERVICE']['swagger_yaml'])
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api(API_YAML, arguments={'title': 'FUJI - FAIR Data Assessment Service'}, validate_responses=False)
-    #app.add_api(API_YAML, arguments={'title': 'FAIRsFAIR Data Assessment Service'},validate_responses=False,pythonic_params=True)
+    #app.add_api(API_YAML, arguments={'title': 'FAIRsFAIR Data Assessment Service'}, validate_responses=False,pythonic_params=True)
     app.run(port=int(config['SERVICE']['service_port']))
 
 if __name__ == '__main__':
@@ -51,7 +53,10 @@ if __name__ == '__main__':
     ini_path = os.path.join(my_path,'config','server.ini')
     config.read(ini_path)
     log_config_path = os.path.join(my_path,'config','logging.ini')
-    log_file_path = os.path.join(my_path, 'logs', 'fuji.log')
+    log_directory = os.path.join(my_path, 'logs')
+    log_file_path = os.path.join(log_directory,'fuji.log')
+    if not os.path.exists(log_directory): # FileHandler will create the log file if it does not exist, not directory
+        os.makedirs(log_directory, exist_ok=True)
     fileConfig(log_config_path, defaults={'logfilename': log_file_path.replace("\\", "/")} )
     logger = logging.getLogger() # use this form to initialize the root logger
     main()
