af71e4fbfb1353debab2f580f14057bd87c1f555
Anusuriya Devaraju <anusuriya_asd@yahoo.com>
2020-05-23 18:41:37 +0200

    file reformatted

--- fuji_server/controllers/fair_check.py
+++ fuji_server/controllers/fair_check.py
@@ -12,7 +12,7 @@ from fuji_server.helper.metadata_collector_datacite import MetaDataCollectorData
 from fuji_server.helper.metadata_collector_dublincore import MetaDataCollectorDublinCore
 from fuji_server.helper.metadata_collector_schemaorg import MetaDataCollectorSchemaOrg
 from fuji_server.helper.metadata_collector import MetaDataCollector
-#from fuji_server.helper.metadata_harvester_oai import OAIMetadataHarvesters
+# from fuji_server.helper.metadata_harvester_oai import OAIMetadataHarvesters
 from fuji_server.helper.metadata_mapper import Mapper
 from fuji_server.helper.preprocessor import Preprocessor
 from fuji_server.helper.repository_helper import RepositoryHelper
@@ -22,7 +22,6 @@ from fuji_server.models import CoreMetadataOutput
 
 
 class FAIRCheck:
-
     METRICS = None
     SPDX_LICENSES = None
     SPDX_LICENSE_NAMES = None
@@ -33,17 +32,17 @@ class FAIRCheck:
         self.pid_url = None  # full pid # e.g., "https://doi.org/10.1594/pangaea.906092 or url (non-pid)
         self.landing_url = None  # url of the landing page of self.pid_url
         self.landing_html = None
-        self.landing_origin = None #schema + authority of the landing page e.g. https://www.pangaea.de
+        self.landing_origin = None  # schema + authority of the landing page e.g. https://www.pangaea.de
         self.pid_scheme = None
         self.logger = logging.getLogger(self.__class__.__name__)
         self.metadata_sources = []
         self.isDebug = test_debug
         self.metadata_merged = {}
-        self.reference_elements = Mapper.REFERENCE_METADATA_LIST.value.copy() # all metadata elements required for FUJI metrics
+        self.reference_elements = Mapper.REFERENCE_METADATA_LIST.value.copy()  # all metadata elements required for FUJI metrics
         if self.isDebug:
             self.msg_filter = MessageFilter()
             self.logger.addFilter(self.msg_filter)
-            self.logger.setLevel(logging.INFO) # set to debug in testing environment
+            self.logger.setLevel(logging.INFO)  # set to debug in testing environment
         self.oaipmh_harvester = None
         FAIRCheck.load_predata()
 
@@ -52,13 +51,13 @@ class FAIRCheck:
         if not cls.METRICS:
             cls.METRICS = Preprocessor.get_custom_metrics(['metric_name', 'total_score'])
         if not cls.SPDX_LICENSES:
-            #cls.SPDX_LICENSES, cls.SPDX_LICENSE_NAMES, cls.SPDX_LICENSE_URLS = Preprocessor.get_licenses()
+            # cls.SPDX_LICENSES, cls.SPDX_LICENSE_NAMES, cls.SPDX_LICENSE_URLS = Preprocessor.get_licenses()
             cls.SPDX_LICENSES, cls.SPDX_LICENSE_NAMES = Preprocessor.get_licenses()
-        #if not cls.DATACITE_REPOSITORIES:
-            #cls.DATACITE_REPOSITORIES = Preprocessor.getRE3repositories()
+        # if not cls.DATACITE_REPOSITORIES:
+        # cls.DATACITE_REPOSITORIES = Preprocessor.getRE3repositories()
 
     @staticmethod
-    def uri_validator(u): #TODO integrate into request_helper.py
+    def uri_validator(u):  # TODO integrate into request_helper.py
         try:
             r = urlparse(u)
             return all([r.scheme, r.netloc])
@@ -111,7 +110,7 @@ class FAIRCheck:
 
             # ======= RETRIEVE METADATA FROM LANDING PAGE =======
             requestHelper: RequestHelper = RequestHelper(self.pid_url, self.logger)
-            requestHelper.setAcceptType(AcceptTypes.html) # request
+            requestHelper.setAcceptType(AcceptTypes.html)  # request
             result = requestHelper.content_negotiate('FsF-F1-02D')
             r = requestHelper.getHTTPResponse()
             if r:
@@ -125,7 +124,7 @@ class FAIRCheck:
                         pid_output.pid = self.id
                         pid_output.pid_scheme = self.pid_scheme
                         pid_result.test_status = 'pass'
-                    pid_output.resolved_url = self.landing_url # url is active, although the identifier is not based on a pid scheme
+                    pid_output.resolved_url = self.landing_url  # url is active, although the identifier is not based on a pid scheme
                     pid_output.resolvable_status = True
             pid_result.score = pid_score
             pid_result.output = pid_output
@@ -139,26 +138,28 @@ class FAIRCheck:
         self.retrieve_metadata(result)
         return uid_result.to_dict(), pid_result.to_dict()
 
-    def retrieve_metadata (self, extruct_metadata):
+    def retrieve_metadata(self, extruct_metadata):
         if extruct_metadata:
             embedded_exists = [k for k, v in extruct_metadata.items() if v]
-            self.logger.info('FsF-F2-01M : Formats of structured metadata embedded in HTML markup {}'.format(embedded_exists))
-            if embedded_exists: # retrieve metadata from landing page
+            self.logger.info(
+                'FsF-F2-01M : Formats of structured metadata embedded in HTML markup {}'.format(embedded_exists))
+            if embedded_exists:  # retrieve metadata from landing page
                 self.retrieve_metadata_embedded(extruct_metadata)
         else:
             self.logger.warning('FsF-F2-01M : No structured metadata embedded in HTML')
 
-        if self.reference_elements: # this will be always true as we need datacite client id
+        if self.reference_elements:  # this will be always true as we need datacite client id
             self.retrieve_metadata_external()
-        self.logger.info('FsF-F2-01M : Type of object described by the metadata - {}'.format(self.metadata_merged.get('object_type')))
+        self.logger.info('FsF-F2-01M : Type of object described by the metadata - {}'.format(
+            self.metadata_merged.get('object_type')))
 
         # retrieve re3metadata based on pid specified
         self.retrieve_re3data()
 
         # validate and instatiate oai-pmh harvester if the endpoint is valie
-        #if self.oaipmh_endpoint:
-            #if (self.uri_validator(self.oaipmh_endpoint)) and self.pid_url:
-                #self.oaipmh_harvester = OAIMetadataHarvesters(endpoint=self.oaipmh_endpoint, resourceidentifier=self.pid_url)
+        # if self.oaipmh_endpoint:
+        # if (self.uri_validator(self.oaipmh_endpoint)) and self.pid_url:
+        # self.oaipmh_harvester = OAIMetadataHarvesters(endpoint=self.oaipmh_endpoint, resourceidentifier=self.pid_url)
 
     def retrieve_re3data(self):
         client_id = self.metadata_merged.get('datacite_client')
@@ -167,13 +168,14 @@ class FAIRCheck:
             repoHelper.lookup_re3data()
 
     def retrieve_metadata_embedded(self, extruct_metadata):
-       # ========= retrieve schema.org (embedded, or from via content-negotiation if pid provided) =========
+        # ========= retrieve schema.org (embedded, or from via content-negotiation if pid provided) =========
         isPid = False
         if self.pid_scheme:
             isPid = True
         ext_meta = extruct_metadata.get('json-ld')
-        schemaorg_collector = MetaDataCollectorSchemaOrg(loggerinst=self.logger, sourcemetadata=ext_meta, mapping=Mapper.SCHEMAORG_MAPPING,
-                                       ispid=isPid, pidurl=self.pid_url)
+        schemaorg_collector = MetaDataCollectorSchemaOrg(loggerinst=self.logger, sourcemetadata=ext_meta,
+                                                         mapping=Mapper.SCHEMAORG_MAPPING,
+                                                         ispid=isPid, pidurl=self.pid_url)
         source_schemaorg, schemaorg_dict = schemaorg_collector.parse_metadata()
         if schemaorg_dict:
             not_null_sco = [k for k, v in schemaorg_dict.items() if v is not None]
@@ -189,8 +191,8 @@ class FAIRCheck:
         # ========= retrieve dublin core embedded in html page =========
         if self.reference_elements:
             dc_collector = MetaDataCollectorDublinCore(loggerinst=self.logger, sourcemetadata=self.landing_html,
-                                                   mapping=Mapper.DC_MAPPING)
-            source_dc, dc_dict= dc_collector.parse_metadata()
+                                                       mapping=Mapper.DC_MAPPING)
+            source_dc, dc_dict = dc_collector.parse_metadata()
             if dc_dict:
                 not_null_dc = [k for k, v in dc_dict.items() if v is not None]
                 self.metadata_sources.append(source_dc)
@@ -205,9 +207,8 @@ class FAIRCheck:
         if self.metadata_merged.get('object_content_identifier') is None:
             links = self.get_html_typed_links()
             if links:
-                self.metadata_merged ['object_content_identifier'] = self.get_html_typed_links()
+                self.metadata_merged['object_content_identifier'] = self.get_html_typed_links()
                 self.metadata_sources.append(MetaDataCollector.Sources.SIGN_POSTING.value)
-
 
     # TODO (important) separate class to represent https://www.iana.org/assignments/link-relations/link-relations.xhtml
     #  use IANA relations for extracting metadata and meaningful links
@@ -218,7 +219,8 @@ class FAIRCheck:
         datalinks = []
         reg_item = '<link rel\s*=\s*\"(item)\"(.*?)href\s*=\s*\"(.*?)\"'
         if self.landing_html != None:
-            header_links_matches = re.findall(reg_item, self.landing_html) # ('item', ' ', 'https://doi.pangaea.de/10.1594/PANGAEA.906092?format=zip')
+            header_links_matches = re.findall(reg_item,
+                                              self.landing_html)  # ('item', ' ', 'https://doi.pangaea.de/10.1594/PANGAEA.906092?format=zip')
             if len(header_links_matches) > 0:
                 for datalink in header_links_matches:
                     datalinks.append(datalink[2])
@@ -266,7 +268,7 @@ class FAIRCheck:
             metadata_status = 'all metadata'
             meta_score.earned = meta_sc
             test_status = 'pass'
-        #elif 1 <= len_found_metadata < len_metadata_required: #TODO - determine the threshold between partial and zero
+        # elif 1 <= len_found_metadata < len_metadata_required: #TODO - determine the threshold between partial and zero
         elif set(partial_elements).issubset(metadata_found):
             metadata_status = 'partial metadata'
             meta_score.earned = meta_sc - 1
@@ -278,9 +280,10 @@ class FAIRCheck:
 
         missing = list(set(metadata_required) - set(metadata_found))
         if missing:
-            self.logger.warning('FsF-F2-01M : Missing core metadata %s' %(missing))
+            self.logger.warning('FsF-F2-01M : Missing core metadata %s' % (missing))
 
-        meta_output: CoreMetadataOutput = CoreMetadataOutput(core_metadata_status=metadata_status, core_metadata_source=self.metadata_sources)
+        meta_output: CoreMetadataOutput = CoreMetadataOutput(core_metadata_status=metadata_status,
+                                                             core_metadata_source=self.metadata_sources)
         meta_output.core_metadata_found = metadata_found
         meta_result.test_status = test_status
         meta_result.score = meta_score
@@ -288,7 +291,6 @@ class FAIRCheck:
         if self.isDebug:
             meta_result.test_debug = self.msg_filter.getMessage(coremeta_identifier)
         return meta_result.to_dict()
-
 
     def check_content_identifier_included(self):
         did_included_identifier = 'FsF-F3-01M'  # FsF-F3-01M: Inclusion of data identifier in metadata
@@ -303,25 +305,27 @@ class FAIRCheck:
         contents = self.metadata_merged.get('object_content_identifier')
         self.logger.info('FsF-F3-01M : Data object identifier specified {}'.format(id_object))
 
-        if FAIRCheck.uri_validator(id_object): # TODO: check if specified identifier same is provided identifier (handle pid and non-pid cases)
+        if FAIRCheck.uri_validator(
+                id_object):  # TODO: check if specified identifier same is provided identifier (handle pid and non-pid cases)
             did_score.earned = did_sc
             did_result.test_status = "pass"
         else:
             self.logger.warning('FsF-F3-01M : Malformed Identifier!')
 
-        content_list=[]
+        content_list = []
         if contents:
             for content_id in contents:
                 self.logger.info('FsF-F3-01M : Data object (content) identifier included {}'.format(content_id))
                 did_output_content = IdentifierIncludedOutputInner()
                 did_output_content.content_identifier_included = content_id
                 try:
-                    urllib.urlopen(content_id) # only check the status, do not download the content
+                    urllib.urlopen(content_id)  # only check the status, do not download the content
                 except urllib.HTTPError as e:
-                    self.logger.warning('FsF-F3-01M : Content identifier {0}, HTTPError code {1} '.format(content_id, e.code))
+                    self.logger.warning(
+                        'FsF-F3-01M : Content identifier {0}, HTTPError code {1} '.format(content_id, e.code))
                 except urllib.URLError as e:
                     self.logger.exception(e.reason)
-                else: # will be executed if there is no exception
+                else:  # will be executed if there is no exception
                     did_output_content.content_identifier_active = True
                     content_list.append(did_output_content)
         else:
@@ -375,12 +379,12 @@ class FAIRCheck:
         html_url = None
         isOsiApproved = False
         for item in FAIRCheck.SPDX_LICENSES:
-            #u = u.lower()
-            #if any(u in v.lower() for v in item.values()):
+            # u = u.lower()
+            # if any(u in v.lower() for v in item.values()):
             seeAlso = item['seeAlso']
             if any(u in v for v in seeAlso):
                 self.logger.info('FsF-R1.1-01M : Found SPDX license representation - {}'.format(item['detailsUrl']))
-                #html_url = '.html'.join(item['detailsUrl'].rsplit('.json', 1))
+                # html_url = '.html'.join(item['detailsUrl'].rsplit('.json', 1))
                 html_url = item['detailsUrl'].replace(".json", ".html")
                 isOsiApproved = item['isOsiApproved']
                 break
@@ -433,19 +437,23 @@ class FAIRCheck:
         searchable_score = FAIRResultCommonScore(total=searchable_sc)
         searchable_output = SearchableOutput()
         search_mechanisms = []
-        sources_registry = [MetaDataCollector.Sources.SCHEMAORG_NEGOTIATE.value, MetaDataCollector.Sources.DATACITE_JSON.value]
+        sources_registry = [MetaDataCollector.Sources.SCHEMAORG_NEGOTIATE.value,
+                            MetaDataCollector.Sources.DATACITE_JSON.value]
         all = str([e.value for e in MetaDataCollector.Sources]).strip('[]')
         self.logger.info('FsF-F4-01M : Metadata is retrieved/extracted through - {}'.format(all))
-        search_engines_support = [MetaDataCollector.Sources.SCHEMAORG_EMBED.value, MetaDataCollector.Sources.DUBLINCORE.value,
+        search_engines_support = [MetaDataCollector.Sources.SCHEMAORG_EMBED.value,
+                                  MetaDataCollector.Sources.DUBLINCORE.value,
                                   MetaDataCollector.Sources.SIGN_POSTING.value]
         # Check search mechanisms based on sources of metadata extracted.
         search_engine_support_match = list(set(self.metadata_sources).intersection(search_engines_support))
         if search_engine_support_match:
-            search_mechanisms.append(OutputSearchMechanisms(mechanism='structured data', mechanism_info=search_engine_support_match))
+            search_mechanisms.append(
+                OutputSearchMechanisms(mechanism='structured data', mechanism_info=search_engine_support_match))
 
         registry_support_match = list(set(self.metadata_sources).intersection(sources_registry))
         if registry_support_match:
-            search_mechanisms.append(OutputSearchMechanisms(mechanism='metadata registry', mechanism_info=registry_support_match))
+            search_mechanisms.append(
+                OutputSearchMechanisms(mechanism='metadata registry', mechanism_info=registry_support_match))
         # TODO (Important) - search via b2find
         length = len(search_mechanisms)
         if length > 0:
@@ -463,4 +471,3 @@ class FAIRCheck:
         if self.isDebug:
             searchable_result.test_debug = self.msg_filter.getMessage(searchable_identifier)
         return searchable_result.to_dict()
-
