33058c21b9c58d05a8ed53608cb54757befbad05
huberrob <huberrob@users.noreply.github.com>
2020-06-12 15:55:50 +0200

    Tried to solve the text/plain issue..

--- fuji_server/helper/request_helper.py
+++ fuji_server/helper/request_helper.py
@@ -8,6 +8,7 @@ from rdflib.plugins.sparql.results.jsonresults import JSONResultSerializer
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class AcceptTypes(Enum):
+    #TODO: this seems to be quite error prone..
     datacite_json = 'application/vnd.datacite.datacite+json'
     datacite_xml = 'application/vnd.datacite.datacite+xml'
     schemaorg = 'application/vnd.schemaorg.ld+json'
@@ -16,9 +17,9 @@ class AcceptTypes(Enum):
     json = 'application/json, text/json;q=0.5'
     jsonld = 'application/ld+json'
     rdfjson = 'application/rdf+json'
-    ntriples = 'text/plain,application/n-triples'
+    nt = 'text/n3, application/n-triples'
     rdfxml = 'application/rdf+xml, text/rdf;q=0.5, application/xml;q=0.1, text/xml;q=0.1'
-    turtle = 'text/turtle, application/turtle, application/x-turtle;q=0.6, text/n3;q=0.3, text/rdf+n3;q=0.3, application/rdf+n3;q=0.3'
+    turtle = 'text/ttl, text/turtle, application/turtle, application/x-turtle;q=0.6, text/n3;q=0.3, text/rdf+n3;q=0.3, application/rdf+n3;q=0.3'
     rdf = 'text/turtle, application/turtle, application/x-turtle;q=0.8, application/rdf+xml, text/n3;q=0.9, text/rdf+n3;q=0.9, application/xhtml+xml;q=0.5, */*;q=0.1'
     default = '*/*'
 
@@ -48,6 +49,7 @@ class RequestHelper:
         return self.parse_response
 
     def content_negotiate(self, metric_id=''):
+        #TODO: not necessarily to be done with the landing page e.g. http://purl.org/vocommons/voaf resolves to a version URL which responds HTML instead of RDF
         self.metric_id=metric_id
         if self.request_url is not None:
             try:
@@ -58,27 +60,46 @@ class RequestHelper:
                     '%s : Content negotiation accept=%s, status=%s ' % (metric_id, self.accept_type, str(status_code)))
                 if status_code == 200:
                     content_type = self.http_response.headers["Content-Type"]
+                    #TODO content type is sometimes wrongly given.. try to infer the type from request
                     if content_type is not None:
-                        content_type = content_type.split(";", 1)[0]
-                        while (True):
-                            for at in AcceptTypes: #e.g., at.name = html, at.value = 'text/html, application/xhtml+xml'
-                                if content_type in at.value:
-                                    if at.name == 'html':
-                                        self.logger.info('%s : Found HTML page!' % metric_id)
-                                        self.parse_response  = self.parse_html(self.http_response.text)
-                                        break
-                                    if at.name == 'xml': # TODO other types (xml)
-                                        self.parse_response  = self.http_response
-                                        break
-                                    if at.name in {'schemaorg', 'json', 'jsonld', 'datacite_json'}:
-                                        self.parse_response  = self.http_response.json()
-                                        # result = json.loads(response.text)
-                                        break
-                                    if at.name in {'rdf', 'rdfjson', 'ntriples', 'rdfxml', 'turtle'}:
-                                        self.parse_response  = self.parse_rdf(self.http_response.text, content_type)
-                                        break
+                        if 'text/plain' in content_type:
+                            self.logger.info('%s : Plain text has been responded as content type!' % metric_id)
+                            #try to find type by url
+                            guessed_format = rdflib.util.guess_format(self.request_url)
+                            if guessed_format is not None:
+                                self.parse_response  = self.parse_rdf(self.http_response.text, guessed_format)
+                                  #content_type = content_type.split(";", 1)[0]
+                        else:
+                            content_type = content_type.split(";", 1)[0]
+                            while (True):
+                                for at in AcceptTypes: #e.g., at.name = html, at.value = 'text/html, application/xhtml+xml'
+                                    if content_type in at.value:
+                                        if at.name == 'html':
+                                            self.logger.info('%s : Found HTML page!' % metric_id)
+                                            self.parse_response  = self.parse_html(self.http_response.text)
+                                            break
+                                        if at.name == 'xml': # TODO other types (xml)
+
+                                            #in case the XML indeed is a RDF:
+                                            # quick one:
+                                            if self.http_response.text.find('<rdf:RDF'):
+                                                self.logger.info('%s : Found RDF document!' % metric_id)
+                                                self.parse_response = self.parse_rdf(self.http_response.text,at.name)
+                                            else:
+                                                self.logger.info('%s : Found XML document!' % metric_id)
+                                                self.parse_response  = self.http_response
+                                            break
+                                        if at.name in {'schemaorg', 'json', 'jsonld', 'datacite_json'}:
+                                            self.parse_response  = self.http_response.json()
+                                            # result = json.loads(response.text)
+                                            break
+                                        if at.name in {'nt','rdf', 'rdfjson', 'ntriples', 'rdfxml', 'turtle'}:
+                                            self.parse_response  = self.parse_rdf(self.http_response.text, content_type)
+                                            break
+
+
                                     # TODO (IMPORTANT) how to handle the rest e.g., text/plain, specify result type
-                            break
+                                break
                # else:
                 #    self.logger.warning('{} : NO successful response received', format(metric_id))
             except requests.exceptions.RequestException as e:
@@ -103,6 +124,7 @@ class RequestHelper:
         # https://rdflib.readthedocs.io/en/stable/apidocs/rdflib.html#rdflib.graph.Graph.parse
         graph = None
         try:
+            self.logger.info('%s : Try to parse RDF %s' % (self.metric_id, self.request_url))
             graph = rdflib.Graph()
             graph.parse(data=response, format=type)
             #queries have to be done in specific metadata collector classes
