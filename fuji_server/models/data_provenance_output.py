7318ce463ef3efce5b6fac8d4e4cacdbcc21b72c
huberrob <huberrob@users.noreply.github.com>
2020-06-16 12:04:03 +0200

    import fixes

--- fuji_server/models/data_provenance_output.py
+++ fuji_server/models/data_provenance_output.py
@@ -5,8 +5,8 @@ from datetime import date, datetime  # noqa: F401
 
 from typing import List, Dict  # noqa: F401
 
-from swagger_server.models.base_model_ import Model
-from swagger_server import util
+from fuji_server.models.base_model_ import Model
+from fuji_server import util
 
 
 class DataProvenanceOutput(Model):
