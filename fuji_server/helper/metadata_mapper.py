fee82a78391e162da400a09172d53b68acd64a42
huberrob <huberrob@users.noreply.github.com>
2020-06-26 14:14:21 +0200

    minor change

--- fuji_server/helper/metadata_mapper.py
+++ fuji_server/helper/metadata_mapper.py
@@ -14,7 +14,7 @@ class Mapper(Enum):
     # datacite_client --> retrieve re3data records
     # TODO include data types of all reference elements
     REFERENCE_METADATA_LIST = ['object_identifier', 'creator', 'title', 'publisher', 'publication_date', 'summary', 'keywords',
-                 'object_content_identifier', 'access_level', 'embargoe', 'access_info', 'policy','related_resources','provenance_general',
+                 'object_content_identifier', 'access_level', 'access_free','policy','related_resources','provenance_general',
                  'measured_variable', 'method', 'creation_date', 'contributor','version', 'license','data_file_format', 'object_type', 'data_size','datacite_client', 'modified_date']
 
     # core metadata elements (FsF-F2-01M)
@@ -23,7 +23,7 @@ class Mapper(Enum):
 
     ## ============================ METADATA MAPPINGS ============================ ##
     #https://www.dublincore.org/webinars/2015/openaire_guidelines_promoting_repositories_interoperability_and_supporting_open_access_funder_mandates/slides.pdf
-    ACCESS_RIGHT_CODES = {'creativecommons': 'public',
+    ACCESS_RIGHT_CODES = {'creativecommons': 'public', 'creative commons': 'public',
                     'c_abf2': 'public', 'c_f1cf': 'embargoed', 'c_16ec': 'restricted', 'c_14cb': 'metadata_only',
                     'OpenAccess': 'public', 'ClosedAccess': 'metadata_only', 'RestrictedAccess': 'restricted',
                     'NON_PUBLIC': 'restricted', 'OP_DATPRO': 'embargoed', 'PUBLIC': 'public',
@@ -33,7 +33,7 @@ class Mapper(Enum):
     # dc: rights, dcterm: accessRights, rightsHolder?
     # license: Recommended practice is to identify the license document with a URI. If this is not possible or feasible, a literal value that identifies the license may be provided.
     DC_MAPPING = {'object_identifier': 'identifier', 'creator': 'creator', 'title': 'title',
-              'publisher': 'publisher', 'publication_date': 'date', 'summary': 'abstract',
+              'publisher': 'publisher', 'publication_date': ['date','available'], 'summary': 'abstract',
               'keywords': 'subject', 'object_type': 'type','modified_date': 'modified',
               'license': 'license', 'data_file_format': 'format', 'access_level':['rights','accessRights'],
                 'related_resources':['relation','source']}
@@ -53,9 +53,10 @@ class Mapper(Enum):
                            'creator_first: creator[*].givenName || author[*].givenName || creator.givenName || author.givenName,' \
                            'creator_last: creator[*].familyName || author[*].familyName || creator.familyName || author.familyName,' \
                            'publisher: publisher.name, license: (license."@id" || license.license."@id") || license, ' \
-                           'summary: description, keywords: keywords, publication_date: datePublished, data_file_format: encodingFormat,' \
+                           'summary: description, keywords: keywords, data_file_format: encodingFormat,' \
                            'object_identifier: (identifier || "@id" || identifier.value ) || (url || url."@id") , ' \
-                            'access_level:  (conditionsOfAccess || isAccessibleForFree || free), '\
+                            'access_level: conditionsOfAccess, ' \
+                            'access_free:  (isAccessibleForFree || free), ' \
                             'measured_variable: variableMeasured[*].name || variableMeasured , '\
                             'related_resources: [{related_resource: isPartOf."@id" || isPartOf.url || isPartOf, relation_type: \'isPartOf\'}, {related_resource: "@reverse".isBasedOn."@id" || "@reverse".isBasedOn.url || isBasedOn , relation_type: \'isBasedOn\'}], ' \
                             'object_content_identifier: (distribution[*].{url: contentUrl, type: (encodingFormat || fileFormat), size: contentSize, profile: schemaVersion} || [distribution.{url: contentUrl, type: (encodingFormat || fileFormat), size: contentSize, profile: schemaVersion}])}'
@@ -65,10 +66,10 @@ class Mapper(Enum):
     DATACITE_JSON_MAPPING = '{object_identifier: id, object_type: types.resourceTypeGeneral,  ' \
                         'creator: creators[*].name, creator_first: creators[*].givenName,' \
                         'creator_last: creators[*].familyName, publisher: publisher, ' \
-                        'title: titles[0].title, keywords: subjects[*].subject, publication_date: dates[?dateType ==\'Available\'].date,' \
+                        'title: titles[0].title, keywords: subjects[*].subject, publication_date: dates[?dateType ==\'Available\'].date || publicationYear,' \
                         'data_size:sizes[0], data_file_format: formats, license: rightsList[*].rightsUri || rightsList[*].rights ,' \
                         'summary: descriptions[?descriptionType == \'Abstract\'].description || descriptions[0].description, ' \
                         'related_resources: relatedIdentifiers[*], datacite_client: clientId, ' \
-                        'modified_date: dates[?dateType == \'Updated\'].date, '\
-                        'object_content_identifier:  {url: contentUrl} , access_level: rights}'
+                        'modified_date: dates[?dateType == \'Updated\'].date,'\
+                        'object_content_identifier:  {url: contentUrl} , access_level: rightsList[*].rightsUri || rightsList[*].rights }'
                         #'related_resources: relatedIdentifiers[*].[relatedIdentifier,relationType]}'
