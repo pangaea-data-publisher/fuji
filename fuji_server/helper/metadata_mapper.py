63d90d4ae73e147621343e87159b94698f76c3fe
huberrob <huberrob@users.noreply.github.com>
2020-09-29 12:19:46 +0200

    updated to master

--- fuji_server/helper/metadata_mapper.py
+++ fuji_server/helper/metadata_mapper.py
@@ -1,10 +1,31 @@
+# MIT License
+#
+# Copyright (c) 2020 PANGAEA (https://www.pangaea.de/)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from enum import Enum
 
 class Mapper(Enum):
     ## ============================ CONFIGURATIONS ============================ ##
     # List of PIDS e.g. those listed in datacite schema
-    VALID_PIDS = ['doi', 'handle', 'ark', 'purl', 'lsid','sra','biosample','ensembl','uniprot','genome']
+    VALID_PIDS = ['doi', 'handle', 'ark', 'purl', 'lsid','sra','biosample','ensembl','uniprot','genome', 'urn']
     #identifiers.org pattern
     #TODO: check if this is needed.. if so ..complete and add check to FAIRcheck
     IDENTIFIERS_PIDS=r'https://identifiers.org/[provider_code/]namespace:accession'
@@ -28,7 +49,7 @@ class Mapper(Enum):
                     'c_abf2': 'public', 'c_f1cf': 'embargoed', 'c_16ec': 'restricted', 'c_14cb': 'metadata_only',
                     'OpenAccess': 'public', 'ClosedAccess': 'closed_metadataonly', 'RestrictedAccess': 'restricted',
                     'NON_PUBLIC': 'restricted', 'OP_DATPRO': 'embargoed', 'PUBLIC': 'public',
-                    'RESTRICTED': 'restricted', 'SENSITIVE': 'embargoed'
+                    'RESTRICTED': 'restricted', 'SENSITIVE': 'restricted', 'embargoedAccess':'embargoed'
                     }
 
     #https://en.wikipedia.org/wiki/List_of_archive_formats#cite_ref-MIME_type_2-3
@@ -56,12 +77,12 @@ class Mapper(Enum):
     SCHEMAORG_MAPPING = '{title: name, object_type: "@type", '\
                             'publication_date: datePublished."@value" || datePublished , '\
                             'modified_date: dateModified."@value" ||dateModified, ' \
-                           'creator: creator[?"@type" ==\'Person\'].name || author[*].name || creator.name || author.name, ' \
+                           'creator: creator[?"@type" ==\'Person\'].name || creator[?"@type" ==\'Organization\'].name || author[*].name || creator.name || author.name, ' \
                            'creator_first: creator[*].givenName || author[*].givenName || creator.givenName || author.givenName,' \
                            'creator_last: creator[*].familyName || author[*].familyName || creator.familyName || author.familyName,' \
                            'contributor: contributor[*].name || contributor[*].familyName, ' \
                            'right_holder: copyrightHolder[*].name || copyrightHolder[*].familyName, ' \
-                           'publisher: publisher.name, license: (license."@id" || license[*].id || license[*].url || license[*].name) || license, ' \
+                           'publisher: publisher.name || provider, license: license."@id" || license[?"@type" ==\'CreativeWork\'].id || license[?"@type" ==\'CreativeWork\'].url || license[?"@type" ==\'CreativeWork\'].name || license, ' \
                            'summary: description, keywords: keywords, ' \
                            'object_identifier: (identifier.value || identifier[0].value || identifier || "@id") || (url || url."@id") , ' \
                             'access_level: conditionsOfAccess, ' \
