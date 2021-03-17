17cc8cdfb2e32022342cfac885c95bdd481c64ea
huberrob <huberrob@users.noreply.github.com>
2021-03-17 15:16:07 +0100

    UUID HASH support #146; maturity level

--- fuji_server/evaluators/fair_evaluator_unique_identifier.py
+++ fuji_server/evaluators/fair_evaluator_unique_identifier.py
@@ -21,8 +21,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import idutils
+import re
+import uuid
 
+import idutils
+import hashid
 from fuji_server.helper.identifier_helper import IdentifierHelper
 from fuji_server.models.uniqueness_output import UniquenessOutput
 from fuji_server.models.uniqueness import Uniqueness
@@ -46,6 +49,7 @@ class FAIREvaluatorUniqueIdentifier(FAIREvaluator):
             self.setEvaluationCriteriumScore('FsF-F1-01D-1',1, 'pass')
             self.output.guid = self.fuji.id
             self.score.earned = self.total_score
+            self.maturity = 3
             # identify main scheme
             found_id = idhelper.preferred_schema
             self.fuji.id_scheme = idhelper.identifier_schemes[0]
@@ -67,8 +71,48 @@ class FAIREvaluatorUniqueIdentifier(FAIREvaluator):
             self.logger.info('FsF-F1-01D : Finalized unique identifier scheme - {}'.format(found_id))
             self.output.guid_scheme = found_id
             self.result.test_status = 'pass'
-            self.result.score = self.score
-            self.result.metric_tests = self.metric_tests
-            self.result.output = self.output
+        elif self.verify_uuid(self.fuji.id):
+            self.logger.log(self.fuji.LOG_SUCCESS,'FsF-F1-01D : Unique identifier (UUID) scheme found')
+            self.setEvaluationCriteriumScore('FsF-F1-01D-2',1, 'pass')
+            self.result.test_status = 'pass'
+            self.output.guid_scheme = 'uuid'
+            self.output.guid = self.fuji.id
+            self.maturity = 1
+            self.score.earned = 1
+        elif self.verify_hash(self.fuji.id):
+            self.logger.log(self.fuji.LOG_SUCCESS,'FsF-F1-01D : Unique identifier (SHA,MD5) scheme found')
+            self.setEvaluationCriteriumScore('FsF-F1-01D-2',1, 'pass')
+            self.result.test_status = 'pass'
+            self.output.guid_scheme = 'hash'
+            self.output.guid = self.fuji.id
+            self.maturity = 1
+            self.score.earned = 1
         else:
+            self.result.test_status = 'fail'
+            self.score.earned = 0
             self.logger.warning('FsF-F1-01D : Failed to check the identifier scheme!.')
+        self.result.score = self.score
+        self.result.metric_tests = self.metric_tests
+        self.result.output = self.output
+        self.result.maturity = self.maturity_levels.get(self.maturity)
+
+    def verify_uuid(self,id):
+        try:
+            uuid_version = uuid.UUID(id).version
+            if uuid_version is not None:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+    def verify_hash(self, id):
+        try:
+            hash = hashid.HashID()
+            validhash = False
+            for hashtype in hash.identifyHash(id):
+                if re.search(r'^(sha|md5|blake)', hashtype.name, re.IGNORECASE):
+                    validhash = True
+            return validhash
+        except Exception:
+            return False
