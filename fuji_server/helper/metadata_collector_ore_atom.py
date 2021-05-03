4d8c21a9b2bf9e9d6b13e1e6d9708b8e0f24702d
huberrob <huberrob@users.noreply.github.com>
2021-05-03 19:50:06 +0200

    oai ore collector

---
+++ fuji_server/helper/metadata_collector_ore_atom.py
@@ -0,0 +1,73 @@
+import logging
+import jmespath
+from fuji_server.helper.metadata_collector import MetaDataCollector
+import feedparser
+# MIT License
+#
+# Copyright (c) 2020 PANGAEA (https://www.pangaea.de/)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+from fuji_server.helper.preprocessor import Preprocessor
+
+from fuji_server.helper.request_helper import RequestHelper, AcceptTypes
+from urlextract import URLExtract
+
+class MetaDataCollectorOreAtom (MetaDataCollector):
+    source_name=None
+    def __init__(self, loggerinst, target_url):
+        #self.is_pid = ispid
+        self.target_url = target_url
+        super().__init__(logger=loggerinst)
+
+
+
+    def parse_metadata(self):
+        ore_metadata= {}
+        if self.target_url:
+            self.source_name = self.getEnumSourceNames().OAI_ORE.value
+            try:
+                feed = feedparser.parse(self.target_url)
+                if feed:
+                    if feed.get('entries'):
+                        if len(feed.get('entries'))==1:
+                            ore_metadata['title'] = feed.get('entries')[0].get('title')
+                            ore_metadata['creator'] = feed.get('entries')[0].get('author')
+                            ore_metadata['publisher'] = feed.get('entries')[0].get('source')
+                            ore_metadata['publication_date'] = feed.get('entries')[0].get('published')
+                            if feed.get('entries')[0].get('source'):
+                                ore_metadata['publisher'] = feed.get('entries')[0].get('source').get('author')
+                            ore_metadata['object_identifier'] = [feed.get('entries')[0].get('id')]
+                            if feed.get('entries')[0].get('link'):
+                                ore_metadata['object_identifier'].append(feed.get('entries')[0].get('link'))
+                            if feed.get('entries')[0].get('link'):
+                                pid = feed.get('entries')[0].get('link')
+                                if pid != self.target_url:
+                                    ore_metadata['object_identifier'] = feed.get('entries')[0].get('link')
+                            if feed.get('entries')[0].get('links'):
+                                ore_metadata['object_content_identifier']=[]
+                                for link in feed.get('entries')[0].get('links'):
+                                    if 'ore/terms/aggregates' in str(link.get('rel')):
+                                        ore_metadata['object_content_identifier'].append({'url': str(link.get('href')), 'type': str(link.get('type')), 'size': str(link.get('length'))})
+            except Exception as err:
+                #print(err.with_traceback())
+                self.logger.info('FsF-F2-01M : Failed to parse OAI ORE XML -: {}'.format(err))
+        else:
+            self.logger.info('FsF-F2-01M : Could not identify OAI ORE metadata')
+
+        return self.source_name, ore_metadata
