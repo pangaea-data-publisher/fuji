2be6eb29db8b3d7d3224221a86b428ebfe686246
huberrob <huberrob@users.noreply.github.com>
2021-06-23 12:20:10 +0200

    catalogue helper for mendeley and google; numeric maturity scale; mappings for several XML standards

---
+++ fuji_server/helper/catalogue_helper_mendeley_data.py
@@ -0,0 +1,40 @@
+import enum
+import logging
+import requests as re
+
+from fuji_server.helper.catalogue_helper import MetaDataCatalogue
+
+
+class MetaDataCatalogueMendeleyData(MetaDataCatalogue):
+
+    islisted = False
+    apiURI = 'https://api.datasearch.elsevier.com/api/v2/search?query='
+    def __init__(self,logger: logging.Logger = None):
+        self.logger = logger
+        self.source = self.getEnumSourceNames().MENDELEY_DATA.value
+
+    def query(self, pidlist):
+        response = None
+        for pid in pidlist:
+            try:
+                if pid:
+                    res= apiresponse = re.get(self.apiURI+'/'+re.utils.quote(str(pid)))
+                    self.logger.info('FsF-F4-01M : Querying Mendeley Data API for -:' + str(pid))
+                    if res.status_code == 200:
+                        resp = res.json()
+                        if resp.get('results'):
+                            for result in resp.get('results'):
+                                if str(pid).lower()  == str(result.get('doi')).lower() or str(pid).lower()  == str(result.get('containerURI')).lower():
+                                    self.islisted =True
+                                    self.logger.info('FsF-F4-01M : Found identifier in Mendeley Data catalogue -:' + str(pid))
+                                    break
+                            if not self.islisted:
+                                self.logger.info('FsF-F4-01M : Identifier not listed in Mendeley Data catalogue -:' + str(pid))
+
+                    else:
+                        self.logger.warning('FsF-F4-01M : Mendeley Data API not available -:'+str(res.status_code))
+            except Exception as e:
+                self.logger.warning('FsF-F4-01M : Mendeley Data API not available or returns errors: '+ str(e))
+
+
+        return response
