cd9203b03d6c3233402a62af1bc9aa1199893e47
huberrob <huberrob@users.noreply.github.com>
2021-07-13 14:41:11 +0200

    workaround for wrong re3data entry bl.imperial

--- fuji_server/helper/preprocessor.py
+++ fuji_server/helper/preprocessor.py
@@ -168,11 +168,13 @@ class Preprocessor(object):
         # retrieve all client id and re3data doi from datacite
         cls.DATACITE_API_REPO = datacite_endpoint
         cls.RE3DATA_API = re3_endpoint
+        #isDebugMode=False
         re3dict_path = os.path.join(cls.fuji_server_dir, 'data', 'repodois.json')
         if isDebugMode:
             with open(re3dict_path) as f:
                 cls.re3repositories = json.load(f)
         else:
+            #print('updating re3data dois')
             p = {'query': 're3data_id:*'}
             try:
                 req = requests.get(datacite_endpoint, params=p, headers=cls.header)
@@ -184,6 +186,8 @@ class Preprocessor(object):
                     for r in response["data"]:
                         cls.re3repositories[r['id']] = r['attributes']['re3data']
                     raw['links'] = response['links']
+                #fix wrong entry
+                cls.re3repositories['bl.imperial']='http://doi.org/10.17616/R3K64N'
                 with open(re3dict_path, 'w') as f2:
                     json.dump(cls.re3repositories, f2)
             except requests.exceptions.RequestException as e:
