36ae73f2e58623025c3aa49cbddfe028da1c3e79
Jens Bröder <j.broeder@fz-juelich.de>
2021-10-14 13:49:33 +0200

    Make create_google_list.py valid python file

--- fuji_server/helper/create_google_lists.py
+++ fuji_server/helper/create_google_lists.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
 from fuji_server.helper.catalogue_helper_google_datasearch import MetaDataCatalogueGoogleDataSearch
 
 g = MetaDataCatalogueGoogleDataSearch()
 
-
 # Step 1 visit: https://www.kaggle.com/googleai/dataset-search-metadata-for-datasets
 # Step 2 download the latest Dataset Search corpus file
 # Step 3 indicate the location of the file below
-google_file_location =
+google_file_location = ''
 
 # Step 4 run the script
 print('Starting to create Google Dataset Search files')
