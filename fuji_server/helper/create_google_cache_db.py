ed392b52dc30f03755c08c47abb37371198289d3
Jens Bröder <j.broeder@fz-juelich.de>
2021-11-10 12:14:30 +0100

    Make create_google_cache_db proper python code

--- fuji_server/helper/create_google_cache_db.py
+++ fuji_server/helper/create_google_cache_db.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from fuji_server.helper.catalogue_helper_google_datasearch import MetaDataCatalogueGoogleDataSearch
 
 g = MetaDataCatalogueGoogleDataSearch()
@@ -6,10 +7,12 @@ g = MetaDataCatalogueGoogleDataSearch()
 # Step 1 visit: https://www.kaggle.com/googleai/dataset-search-metadata-for-datasets
 # Step 2 download the latest Dataset Search corpus file
 # Step 3 indicate the location of the file below
-google_file_location =
+google_file_location = None
 
 # Step 4 run the script
-print('Starting to create Google Dataset Search DB')
-g.create_cache_db(google_file_location)
-
-print('Finished...')
+if google_file_location is not None:
+    print('Starting to create Google Dataset Search DB')
+    g.create_cache_db(google_file_location)
+    print('Finished...')
+else:
+    print('No google_file_location provided, FUJI is not properly installed.')
