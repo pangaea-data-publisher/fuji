894127f54707e80105c99c8a112dd0d531bea6f8
huberrob <huberrob@users.noreply.github.com>
2022-09-23 14:27:29 +0200

    version 2.2.0

--- fuji_server/controllers/fair_check.py
+++ fuji_server/controllers/fair_check.py
@@ -107,7 +107,7 @@ class FAIRCheck:
     GOOGLE_DATA_DOI_CACHE = []
     GOOGLE_DATA_URL_CACHE = []
     LINKED_VOCAB_INDEX = {}
-    FUJI_VERSION = '2.1.1'
+    FUJI_VERSION = '2.2.0'
 
     def __init__(self,
                  uid,
