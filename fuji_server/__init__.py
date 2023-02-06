56b0af7e3aac89637755d8fa61fb4d0df0006781
huberrob <huberrob@users.noreply.github.com>
2023-02-06 19:40:38 +0100

    changed default accept header see #321 ; version 2.2.5

--- fuji_server/__init__.py
+++ fuji_server/__init__.py
@@ -31,4 +31,4 @@ from fuji_server.models.searchable_output import SearchableOutput
 from fuji_server.models.uniqueness import Uniqueness
 from fuji_server.models.uniqueness_output import UniquenessOutput
 
-__version__ = '2.2.4'
+__version__ = '2.2.5'
