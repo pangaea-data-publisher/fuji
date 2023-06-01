8403aa7bd0ce1f1593ec4c6fcc1a9b01304b123f
huberrob <huberrob@users.noreply.github.com>
2023-06-01 12:46:35 +0200

    rewrote and adopted evaluator_file_format to new test modularisation and community metrics support

--- fuji_server/evaluators/fair_evaluator_file_format.py
+++ fuji_server/evaluators/fair_evaluator_file_format.py
@@ -43,16 +43,12 @@ class FAIREvaluatorFileFormat(FAIREvaluator):
         or in open format (see e.g., https://en.wikipedia.org/wiki/List_of_open_formats) or in a scientific file format.
     """
 
-    def evaluate(self):
+    def __init__(self, fuji_instance):
+        FAIREvaluator.__init__(self, fuji_instance)
+        self.set_metric('FsF-R1.3-02D')
+        self.data_file_list=[]
 
-        text_format_regex = r'(^text)[\/]|[\/\+](xml|text|json)'
-
-        self.result = DataFileFormat(id=self.metric_number,
-                                     metric_identifier=self.metric_identifier,
-                                     metric_name=self.metric_name)
-        self.output = DataFileFormatOutput()
-        data_file_list = []
-
+    def setFileFormatDict(self):
         if not self.fuji.content_identifier:  # self.content_identifier only includes uris that are accessible
             contents = self.fuji.metadata_merged.get('object_content_identifier')
             unique_types = []
@@ -109,80 +105,99 @@ class FAIREvaluatorFileFormat(FAIREvaluator):
                                 for tika_mime in self.fuji.tika_content_types_list:
                                     if tika_mime != 'unverified' and tika_mime not in mime_url_pair:
                                         mime_url_pair[tika_mime] = data_file.get('url')
+        return mime_url_pair
 
-            # FILE FORMAT CHECKS....
-            # check if format is a scientific one:
-            preferred_mimetype = None
-            for mimetype, url in mime_url_pair.items():
-                data_file_output = DataFileFormatOutputInner()
-                preferance_reason = []
-                subject_area = []
-                if mimetype in self.fuji.SCIENCE_FILE_FORMATS:
-                    self.setEvaluationCriteriumScore('FsF-R1.3-02D-1c', 0, 'pass')
-                    if self.maturity < 3:
-                        self.maturity = 3
-                    if self.fuji.SCIENCE_FILE_FORMATS.get(mimetype) == 'Generic':
-                        subject_area.append('General')
-                        preferance_reason.append('generic science format')
-                    else:
-                        subject_area.append(self.fuji.SCIENCE_FILE_FORMATS.get(mimetype))
-                        preferance_reason.append('science format')
-                    data_file_output.is_preferred_format = True
-                # check if long term format
-                if mimetype in self.fuji.LONG_TERM_FILE_FORMATS:
-                    self.setEvaluationCriteriumScore('FsF-R1.3-02D-1b', 0, 'pass')
-                    if self.maturity < 2:
-                        self.maturity = 2
-                    preferance_reason.append('long term format')
-                    subject_area.append('General')
-                    data_file_output.is_preferred_format = True
-                # check if open format
-                if mimetype in self.fuji.OPEN_FILE_FORMATS:
-                    self.setEvaluationCriteriumScore('FsF-R1.3-02D-1a', 0, 'pass')
-                    if self.maturity < 1:
-                        self.maturity = 1
-                    preferance_reason.append('open format')
-                    subject_area.append('General')
-                    data_file_output.is_preferred_format = True
-                # generic text/xml/json file check
 
-                if 'html' not in mimetype and re.search(text_format_regex, mimetype):
-                    self.setEvaluationCriteriumScore('FsF-R1.3-02D-1a', 0, 'pass')
-                    self.setEvaluationCriteriumScore('FsF-R1.3-02D-1b', 0, 'pass')
-                    self.setEvaluationCriteriumScore('FsF-R1.3-02D-1c', 0, 'fail')
-                    self.maturity = 2
-                    preferance_reason.extend(['long term format', 'open format', 'generic science format'])
-                    subject_area.append('General')
-                    data_file_output.is_preferred_format = True
-                if 'html' in mimetype:
+    def testCommunityFileFormatUsed(self, mime_url_pair):
+        # FILE FORMAT CHECKS....
+        # check if format is a scientific one:
+        preferred_mimetype = None
+        data_file_list = []
+        text_format_regex = r'(^text)[\/]|[\/\+](xml|text|json)'
+        test_status = False
+        if self.isTestDefined(self.metric_identifier + '-1'):
+            test_score = self.getTestConfigScore(self.metric_identifier + '-1')
+            if mime_url_pair:
+                for mimetype, url in mime_url_pair.items():
+                    data_file_output = DataFileFormatOutputInner()
                     preferance_reason = []
+                    subject_area = []
+                    if mimetype in self.fuji.SCIENCE_FILE_FORMATS:
+                        self.setEvaluationCriteriumScore('FsF-R1.3-02D-1c', 0, 'pass')
+                        if self.maturity < self.getTestConfigMaturity('FsF-R1.3-02D-1c'):
+                            self.maturity = self.getTestConfigMaturity('FsF-R1.3-02D-1c')
+                        if self.fuji.SCIENCE_FILE_FORMATS.get(mimetype) == 'Generic':
+                            subject_area.append('General')
+                            preferance_reason.append('generic science format')
+                        else:
+                            subject_area.append(self.fuji.SCIENCE_FILE_FORMATS.get(mimetype))
+                            preferance_reason.append('science format')
+                        test_status = data_file_output.is_preferred_format = True
 
-                if preferance_reason:
-                    preferred_detected = True
-                    preferred_mimetype = mimetype
+                    # check if long term format
+                    if mimetype in self.fuji.LONG_TERM_FILE_FORMATS:
+                        self.setEvaluationCriteriumScore('FsF-R1.3-02D-1b', 0, 'pass')
+                        if self.maturity < self.getTestConfigMaturity('FsF-R1.3-02D-1b'):
+                            self.maturity = self.getTestConfigMaturity('FsF-R1.3-02D-1b')
+                        preferance_reason.append('long term format')
+                        subject_area.append('General')
+                        test_status = data_file_output.is_preferred_format = True
+                    # check if open format
+                    if mimetype in self.fuji.OPEN_FILE_FORMATS:
+                        self.setEvaluationCriteriumScore('FsF-R1.3-02D-1a', 0, 'pass')
+                        if self.maturity < self.getTestConfigMaturity('FsF-R1.3-02D-1a'):
+                            self.maturity = self.getTestConfigMaturity('FsF-R1.3-02D-1a')
+                        preferance_reason.append('open format')
+                        subject_area.append('General')
+                        test_status = data_file_output.is_preferred_format = True
+                    # generic text/xml/json file check
 
-                data_file_output.mime_type = mimetype
-                data_file_output.file_uri = url
-                data_file_output.preference_reason = list(set(preferance_reason))
-                data_file_output.subject_areas = list(set(subject_area))
-                data_file_list.append(data_file_output)
+                    if 'html' not in mimetype and re.search(text_format_regex, mimetype):
+                        self.setEvaluationCriteriumScore('FsF-R1.3-02D-1a', 0, 'pass')
+                        self.setEvaluationCriteriumScore('FsF-R1.3-02D-1b', 0, 'pass')
+                        self.setEvaluationCriteriumScore('FsF-R1.3-02D-1c', 0, 'fail')
+                        self.maturity = self.getTestConfigMaturity('FsF-R1.3-02D-1b')
+                        preferance_reason.extend(['long term format', 'open format', 'generic science format'])
+                        subject_area.append('General')
+                        test_status = data_file_output.is_preferred_format = True
+                    if 'html' in mimetype:
+                        preferance_reason = []
 
-            if preferred_detected:
-                self.score.earned = 1
-                self.setEvaluationCriteriumScore('FsF-R1.3-02D-1', 1, 'pass')
-                #self.maturity = 3
-                self.logger.log(
-                    self.fuji.LOG_SUCCESS,
-                    'FsF-R1.3-02D : Could identify a file format commonly used by the scientific community -:' +
-                    str(preferred_mimetype))
-                self.result.test_status = 'pass'
+                    if preferance_reason:
+                        preferred_detected = True
+                        preferred_mimetype = mimetype
+                        self.logger.log(
+                            self.fuji.LOG_SUCCESS,
+                            'FsF-R1.3-02D : Could identify a file format commonly used by the scientific community -:' +
+                            str(preferred_mimetype))
+                    data_file_output.mime_type = mimetype
+                    data_file_output.file_uri = url
+                    data_file_output.preference_reason = list(set(preferance_reason))
+                    data_file_output.subject_areas = list(set(subject_area))
+                    self.data_file_list.append(data_file_output)
+        if test_status:
+            self.score.earned = test_score
+            self.setEvaluationCriteriumScore('FsF-R1.3-02D-1', test_score, 'pass')
+        return test_status
+
+    def evaluate(self):
+
+        self.result = DataFileFormat(id=self.metric_number,
+                                     metric_identifier=self.metric_identifier,
+                                     metric_name=self.metric_name)
+        self.output = DataFileFormatOutput()
+
+
+        mime_url_dict = self.setFileFormatDict()
+        if self.testCommunityFileFormatUsed(mime_url_dict):
+            self.result.test_status = 'pass'
         else:
             self.logger.warning(
                 'FsF-R1.3-02D : Could not perform file format checks as data content identifier(s) unavailable/inaccesible'
             )
             self.result.test_status = 'fail'
 
-        self.output = data_file_list
+        self.output = self.data_file_list
         self.result.output = self.output
         self.result.metric_tests = self.metric_tests
         self.result.maturity = self.maturity
