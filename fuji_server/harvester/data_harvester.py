f419cabdb89830c75798f85e69decdefa2e994da
huberrob <huberrob@users.noreply.github.com>
2023-11-07 09:24:40 +0100

    Improved threaded download see #450; limit number of files to download to five per mime type (given in metadata); init dict variables to avoid string type data size values; Improved threaded download of data files; data info collection (for data dict) now also done in thread;

--- fuji_server/harvester/data_harvester.py
+++ fuji_server/harvester/data_harvester.py
@@ -1,7 +1,7 @@
 import io
 import os
 import re
-import time
+import threading
 import urllib
 
 import idutils
@@ -22,9 +22,12 @@ class DataHarvester:
         self.metrics = metrics
         self.timeout = 10
         self.max_download_size = 1000000
+        self.max_number_per_mime = 5
         self.data = {}
         self.landing_page = landing_page
         self.content_type = None
+        self.delay_time = 3
+        self.responses = {}
 
     def expand_url(self, url):
         # replace local urls with full path from landing_page URI
@@ -49,140 +52,77 @@ class DataHarvester:
                     mime_list.append(str(xm[0] + "/" + xm[1]))
         return mime_list
 
-    def retrieve_all_data_old(self, howmany=1):
-        if self.data_links:
-            if isinstance(self.data_links, list):
-                self.data_links.reverse()
-                if len(self.data_links) > howmany:
-                    self.logger.warning(
-                        "FsF-R1-01MD : Will not use all given data links, will use "
-                        + str(howmany)
-                        + " accessible data files from "
-                        + (str(len(self.data_links)))
-                    )
-                inx = 0  # number of files which are not accessible (status code !=200)
-                for idx, datafile in enumerate(self.data_links):
-                    fileinfo = {}
-                    if datafile.get("url") and idx < howmany + inx:
-                        fileinfo, buffer = self.get(datafile.get("url"))
-                        if fileinfo.get("status_code") != 200:
-                            inx += 1
-                        else:
-                            fileinfo["verified"] = True
-                        fileinfo["tika_content_type"] = []
-                        if fileinfo:
-                            fileinfo.update(self.tika(buffer, datafile.get("url")))
-                    else:
-                        fileinfo["verified"] = False
-                    fileinfo["claimed_size"] = datafile.get("size")
-                    fileinfo["claimed_type"] = datafile.get("type")
-                    fileinfo["url"] = datafile.get("url")
-                    self.data[datafile.get("url")] = fileinfo
-        return True
-
-    def retrieve_all_data(self):
+    def retrieve_all_data(self, scan_content=True):
         # TODO: prioritise scientific files which can be opened by tika or other parsers for content analysis
-        # rchoose sample of data_links which are accessible the smallest file per mime type (onbe per mime type)
+        # choose sample of data_links which are accessible the smallest file per mime type (onbe per mime type)
+        sorted_files = {}
+        urls_to_check = {}
+        # sort data links, here we trust the mime type given by the server
         if self.data_links:
             if isinstance(self.data_links, list):
-                sorted_files = {}
-                for f in self.data_links:
-                    if sorted_files.get(f.get("type")):
-                        sorted_files[f.get("type")].append(f)
-                        sorted_files[f.get("type")] = sorted(
-                            sorted_files[f.get("type")], key=lambda d: d["size"] if d.get("size") else float("inf")
+                for fl in self.data_links:
+                    if fl.get("size"):
+                        try:
+                            fl["size"] = int(fl["size"])
+                        except:
+                            fl["size"] = None
+                    if sorted_files.get(fl.get("type")):
+                        sorted_files[fl.get("type")].append(fl)
+                        sorted_files[fl.get("type")] = sorted(
+                            sorted_files[fl.get("type")], key=lambda d: d["size"] if d.get("size") else float("inf")
                         )
                     else:
-                        sorted_files[f.get("type")] = [f]
-                for ft in sorted_files.values():
-                    for datafile in ft:
-                        fileinfo, buffer = self.get(datafile.get("url"))
-                        fileinfo["verified"] = False
-                        if fileinfo.get("status_code") == 200:
-                            fileinfo["verified"] = True
-                        fileinfo["tika_content_type"] = []
-                        if fileinfo:
-                            fileinfo.update(self.tika(buffer, datafile.get("url")))
-                        else:
-                            fileinfo["verified"] = False
-                        fileinfo["claimed_size"] = datafile.get("size")
-                        fileinfo["claimed_type"] = datafile.get("type")
-                        fileinfo["url"] = datafile.get("url")
-                        # if fileinfo["verified"]:
-                        self.data[datafile.get("url")] = fileinfo
-            # print('DATA', self.data)
+                        sorted_files[fl.get("type")] = [fl]
+        # threaded download starts here
+        for fmime, ft in sorted_files.items():
+            timeout = 10
+            if len(ft) > self.max_number_per_mime:
+                self.logger.warning(
+                    f"FsF-F3-01M : Found more than -: {str(self.max_number_per_mime)} data links ({str(len(ft))}) of type {fmime} will only take {str(self.max_number_per_mime)}"
+                )
+
+            for f in ft[: self.max_number_per_mime]:
+                if f.get("url"):
+                    urls_to_check[f.get("url")] = f
+
+            # urls_to_check.extend([f.get('url') for f in ft[:self.max_number_per_mime]])
+            # urls = [f.get('url') for f in ft[:self.max_number_per_mime]]
+        e = threading.Event()
+        if urls_to_check:
+            # urls_to_check =list(set([u for u in urls_to_check if u]))
+            threads = [
+                threading.Thread(target=self.get_url_data_and_info, args=(url, timeout))
+                for url in urls_to_check.values()
+            ]
+            for thread in threads:
+                thread.start()
+            for ti, thread in enumerate(threads):
+                if ti > 0:
+                    timeout = 1
+                thread.join(timeout)
+                if thread.is_alive():
+                    e.set()
+                thread.join()
+                # thread ends here
+                # print(self.data)
         return True
 
-    def get(self, url):
-        start = time.time()
-        fileinfo = {}
-        downloaded_size = 0
-        file_buffer_object = io.BytesIO()
-        content_size = 0
+    def get_url_data_and_info(self, urldict, timeout):
+        header = {"Accept": "*/*", "User-Agent": "F-UJI"}
+        if self.auth_token:
+            header["Authorization"] = self.auth_token_type + " " + self.auth_token
+        # header["Range"] = "bytes=0-" + str(self.max_download_size)
+        url = urldict.get("url")
+        urldict.get("size")
         if url:
-            fileinfo["is_persistent"] = False
-            idhelper = IdentifierHelper(url)
-            if idhelper.preferred_schema:
-                fileinfo["schema"] = idhelper.preferred_schema
-            if idhelper.is_persistent:
-                fileinfo["is_persistent"] = True
             if not idutils.is_url(url):
                 url = self.expand_url(url)
+            print("Downloading.. ", url)
             try:
-                request_headers = {"Accept": "*/*", "User-Agent": "F-UJI"}
-                if self.auth_token:
-                    request_headers["Authorization"] = self.auth_token_type + " " + self.auth_token
-                request = urllib.request.Request(url, headers=request_headers)
-                response = urllib.request.urlopen(request, timeout=self.timeout)
-                rstatus = response.getcode()
-                fileinfo["status_code"] = rstatus
-                fileinfo["truncated"] = False
-                fileinfo["resolved_url"] = response.geturl()
-                # self.info['response_content_type'] = content_type = response.info().get_content_type()
-                if response.headers.get("content-type"):
-                    self.content_type = fileinfo["header_content_type"] = response.headers.get("content-type").split(
-                        ";"
-                    )[0]
-                elif response.headers.get("Content-Type"):
-                    self.content_type = fileinfo["header_content_type"] = response.headers.get("Content-Type").split(
-                        ";"
-                    )[0]
-
-                if response.headers.get("content-length"):
-                    fileinfo["header_content_size"] = response.headers.get("content-length").split(";")[0]
-                elif response.headers.get("Content-Length"):
-                    fileinfo["header_content_size"] = response.headers.get("Content-Length").split(";")[0]
-
-                chunksize = 1024
-                content_size = 0
-                while True:
-                    chunk = response.read(chunksize)
-                    if not chunk:
-                        break
-                    else:
-                        # response_body.append(chunk)
-                        file_buffer_object.write(chunk)
-                        # avoiding large file sizes to test with TIKA.. truncate after 1 Mb
-                        fileinfo["downloaded_size"] = downloaded_size = downloaded_size + len(chunk)
-                        if time.time() > (start + self.timeout) or downloaded_size >= self.max_download_size:
-                            self.logger.warning(
-                                "FsF-R1-01MD : File too large.., skipped download after -:"
-                                + str(self.timeout)
-                                + " sec or receiving > "
-                                + str(self.max_download_size)
-                                + f"- {url}"
-                            )
-                            content_size = 0
-                            content_size = str(response.headers.get("content-length")).split(";")[0]
-                            fileinfo["truncated"] = True
-
-                            break
-                        else:
-                            content_size = downloaded_size
-                        fileinfo["content_size"] = content_size
-                response.close()
-                self.logger.warning(f"FsF-R1-01MD : Content identifier accessible -: {url}, HTTPStatus code {rstatus} ")
-
+                request = urllib.request.Request(url, headers=header)
+                response = urllib.request.urlopen(request, timeout=timeout)
+                self.responses[url] = response
+                self.set_data_info(urldict, response)
             except urllib.error.HTTPError as e:
                 self.logger.warning(f"FsF-F3-01M : Content identifier inaccessible -: {url}, HTTPError code {e.code} ")
                 self.logger.warning(f"FsF-R1-01MD : Content identifier inaccessible -: {url}, HTTPError code {e.code} ")
@@ -204,7 +144,52 @@ class DataHarvester:
                 self.logger.warning("FsF-F3-01M : Content identifier inaccessible -:" + url + " " + str(e))
                 self.logger.warning("FsF-R1-01MD : Content identifier inaccessible -:" + url + " " + str(e))
                 self.logger.warning("FsF-R1.3-02D : Content identifier inaccessible -:" + url + " " + str(e))
-        return fileinfo, file_buffer_object
+
+    def set_data_info(self, urldict, response):
+        fileinfo = {}
+        if isinstance(urldict, dict):
+            fileinfo = {
+                "url": urldict.get("url"),
+                "claimed_size": urldict.get("size"),
+                "claimed_type": urldict.get("type"),
+                "truncated": False,
+                "is_persistent": False,
+            }
+            file_buffer_object = io.BytesIO()
+            rstatus = response.getcode()
+            idhelper = IdentifierHelper(urldict.get("url"))
+            if idhelper.preferred_schema:
+                fileinfo["schema"] = idhelper.preferred_schema
+            if idhelper.is_persistent:
+                fileinfo["is_persistent"] = True
+            fileinfo["status_code"] = rstatus
+            fileinfo["verified"] = False
+            if fileinfo.get("status_code") == 200:
+                fileinfo["verified"] = True
+
+            fileinfo["resolved_url"] = response.geturl()
+            if response.headers.get("content-type"):
+                self.content_type = fileinfo["header_content_type"] = response.headers.get("content-type").split(";")[0]
+            elif response.headers.get("Content-Type"):
+                self.content_type = fileinfo["header_content_type"] = response.headers.get("Content-Type").split(";")[0]
+            if response.headers.get("content-length"):
+                fileinfo["header_content_size"] = response.headers.get("content-length").split(";")[0]
+            elif response.headers.get("Content-Length"):
+                fileinfo["header_content_size"] = response.headers.get("Content-Length").split(";")[0]
+            try:
+                fileinfo["header_content_size"] = int(fileinfo["header_content_size"])
+            except:
+                fileinfo["header_content_size"] = self.max_download_size
+                pass
+            content = response.read(self.max_download_size)
+            file_buffer_object.write(content)
+            fileinfo["content_size"] = file_buffer_object.getbuffer().nbytes
+            if fileinfo["content_size"] < fileinfo["header_content_size"]:
+                fileinfo["truncated"] = True
+            if fileinfo["content_size"] > 0:
+                fileinfo.update(self.tika(file_buffer_object, urldict.get("url")))
+            self.data[urldict.get("url")] = fileinfo
+        return fileinfo
 
     def tika(self, file_buffer_object, url):
         parsed_content = ""
@@ -214,11 +199,6 @@ class DataHarvester:
         try:
             if len(file_buffer_object.getvalue()) > 0:
                 parsedFile = parser.from_buffer(file_buffer_object.getvalue())
-                # print('TIKA: ',url, parsedFile)
-                # if not parsedFile.get('content'):
-                #    print('NO CONTENT')
-                # else:
-                #    print('HAS CONTENT')
                 fileinfo["tika_status"] = status = parsedFile.get("status")
                 tika_content_types = parsedFile.get("metadata").get("Content-Type")
                 parsed_content = parsedFile.get("content")
