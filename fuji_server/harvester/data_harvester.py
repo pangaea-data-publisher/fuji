670e25e2f1b305516ce7665027fa7c0a1bec11f3
huberrob <huberrob@users.noreply.github.com>
2023-11-10 11:07:16 +0100

    disabled print, changed log message

--- fuji_server/harvester/data_harvester.py
+++ fuji_server/harvester/data_harvester.py
@@ -109,7 +109,7 @@ class DataHarvester:
             timeout = 10
             if len(ft) > self.max_number_per_mime:
                 self.logger.warning(
-                    f"FsF-F3-01M : Found more than -: {str(self.max_number_per_mime)} data links ({str(len(ft))}) of type {fmime} will only take {str(self.max_number_per_mime)}"
+                    f"FsF-F3-01M : Found more than -: {str(self.max_number_per_mime)} data links (out of {str(len(ft))}) of type {fmime} will only take {str(self.max_number_per_mime)}"
                 )
             files_to_check = ft[: self.max_number_per_mime]
             # add the fifth one for compatibility reasons < f-uji 3.0.1, when we took the last of list of length FILES_LIMIT
@@ -154,7 +154,7 @@ class DataHarvester:
         if url:
             if not idutils.is_url(url):
                 url = self.expand_url(url)
-            print("Downloading.. ", url)
+            # print("Downloading.. ", url)
             response = None
             try:
                 request = urllib.request.Request(url, headers=header)
