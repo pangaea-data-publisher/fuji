4fee56462dcbe9a1262d152be0932641bcb2ac68
Kara Moraw <k.moraw@epcc.ed.ac.uk>
2023-11-27 15:19:06 +0000

    metric version configuration added to simpleclient

--- simpleclient/index.php
+++ simpleclient/index.php
@@ -47,6 +47,7 @@ set_time_limit(0);
 $fuji_server = 'http://localhost:1071/fuji/api/v1/evaluate';
 $fuji_username = 'yourusername';
 $fuji_password = 'yourpassword';
+$metric_version = "metrics_v0.5";  #"metrics_v0.7_software";
 ################################################################
 
 $fair_basic_terms=['F'=>'Findable','A'=>'Accessible','I'=>'Interoperable','R'=>'Reusable'];
@@ -171,6 +172,7 @@ if (isset($input_pid)) {
     $message->metadata_service_type = $input_service_type;
     $message->test_debug = true;
     $message->use_datacite = $usedatacite;
+    $message->metric_version = $metric_version;
     $post = json_encode($message);
 
     $username = $fuji_username;
