3f59208f9ed9e29c4d9945369b53c7c5a0669e3e
Kara Moraw <k.moraw@epcc.ed.ac.uk>
2024-01-22 12:40:56 +0000

    #23 added 6+17 to data_provenance, TODO should separate

--- fuji_server/evaluators/fair_evaluator_data_provenance.py
+++ fuji_server/evaluators/fair_evaluator_data_provenance.py
@@ -28,10 +28,35 @@ class FAIREvaluatorDataProvenance(FAIREvaluator):
         FAIREvaluator.__init__(self, fuji_instance)
         self.set_metric(["FsF-R1.2-01M", "FRSM-06-F2", "FRSM-17-R1.2"])
 
+        self.metric_test_map = {  # overall map
+            "testProvenanceMetadataAvailable": ["FsF-R1.2-01M-1", "FRSM-06-F2-1"],
+            "testProvenanceStandardsUsed": ["FsF-R1.2-01M-2"],
+            "testCitationMetadata": ["FRSM-06-F2-2"],
+            "testProportionalCredit": ["FRSM-06-F2-3"],
+            "testFilesPresent": ["FRSM-06-F2-CESSDA-1"],
+            "testORCIDInZenodoAndCitationFile": ["FRSM-06-F2-CESSDA-2"],
+            "testCommitHistory": ["FRSM-17-R1.2-1", "FRSM-17-R1.2-CESSDA-1"],
+            "testCommitLinkedToIssue": ["FRSM-17-R1.2-2"],
+            "testOtherProvenanceTools": ["FRSM-17-R1.2-3"],
+            "testIssueLinkedToBranch": ["FRSM-17-R1.2-CESSDA-2"],
+            "testPRLinkedToIssue": ["FRSM-17-R1.2-CESSDA-3"],
+        }
+
     def testProvenanceMetadataAvailable(self):
+        agnostic_test_name = "testProvenanceMetadataAvailable"
         test_status = False
-        if self.isTestDefined(self.metric_identifier + "-1"):
-            test_score = self.getTestConfigScore(self.metric_identifier + "-1")
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        # TODO implement
+        if test_id.startswith("FRSM"):
+            self.logger.warning(
+                f"{self.metric_identifier} : Test for descriptive metadata is not implemented for FRSM."
+            )
+        if test_defined:
+            test_score = self.getTestConfigScore(test_id)
             provenance_metadata_output = DataProvenanceOutputInner()
             provenance_metadata_output.provenance_metadata = []
             provenance_metadata_output.is_available = False
@@ -75,16 +100,22 @@ class FAIREvaluatorDataProvenance(FAIREvaluator):
                     self.fuji.LOG_SUCCESS,
                     self.metric_identifier + " : Found data creation-related provenance information",
                 )
-                self.maturity = self.getTestConfigMaturity(self.metric_identifier + "-1")
+                self.maturity = self.getTestConfigMaturity(test_id)
                 self.score.earned = test_score
-                self.setEvaluationCriteriumScore(self.metric_identifier + "-1", test_score, "pass")
+                self.setEvaluationCriteriumScore(test_id, test_score, "pass")
             self.output.provenance_metadata_included = provenance_metadata_output
         return test_status
 
     def testProvenanceStandardsUsed(self):
+        agnostic_test_name = "testProvenanceStandardsUsed"
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
         test_status = False
-        if self.isTestDefined(self.metric_identifier + "-2"):
-            test_score = self.getTestConfigScore(self.metric_identifier + "-2")
+        if test_defined:
+            test_score = self.getTestConfigScore(test_id)
             provenance_namespaces = [
                 "http://www.w3.org/ns/prov",
                 "http://www.w3.org/ns/prov#",
@@ -106,14 +137,175 @@ class FAIREvaluatorDataProvenance(FAIREvaluator):
                 structured_metadata_output.is_available = True
                 for used_prov_ns in used_provenance_namespace:
                     structured_metadata_output.provenance_metadata.append({"namespace": used_prov_ns})
-                self.setEvaluationCriteriumScore(self.metric_identifier + "-2", test_score, "pass")
-                self.maturity = self.getTestConfigMaturity(self.metric_identifier + "-2")
+                self.setEvaluationCriteriumScore(test_id, test_score, "pass")
+                self.maturity = self.getTestConfigMaturity(test_id)
                 self.logger.log(
                     self.fuji.LOG_SUCCESS, self.metric_identifier + " : Found use of dedicated provenance ontologies"
                 )
             else:
                 self.logger.warning(self.metric_identifier + " : Formal provenance metadata is unavailable")
             self.output.structured_provenance_available = structured_metadata_output
+        return test_status
+
+    def testCitationMetadata(self):
+        """The software includes citation metadata that includes all contributors and their roles. This includes ORCIDs when contributors have them.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testCitationMetadata"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(f"{self.metric_identifier} : Test for citation metadata is not implemented.")
+        return test_status
+
+    def testProportionalCredit(self):
+        """Does the citation metadata include the proportional credit attributed to each contributor?
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testProportionalCredit"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(
+                f"{self.metric_identifier} : Test for proportional credit in citation metadata is not implemented."
+            )
+        return test_status
+
+    def testFilesPresent(self):
+        """A CITATION and/or CONTRIBUTORS files is present in the root of the repository.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testFilesPresent"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(
+                f"{self.metric_identifier} : Test for presence of CITATION or CONTRIBUTORS file is not implemented."
+            )
+        return test_status
+
+    def testORCIDInZenodoAndCitationFile(self):
+        """Author details (including ORCIDs) are present in the corresponding Zenodo record. ORCIDs are present for authors in the CITATION.cff file.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testORCIDInZenodoAndCitationFile"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(
+                f"{self.metric_identifier} : Test for author ORCIDs in CITATION file and Zenodo record is not implemented."
+            )
+        return test_status
+
+    def testCommitHistory(self):
+        """The software source code repository / forge includes a commit history.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testCommitHistory"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(f"{self.metric_identifier} : Test for commit history is not implemented.")
+        return test_status
+
+    def testCommitLinkedToIssue(self):
+        """The software source code repository links commits to issues / tickets.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testCommitLinkedToIssue"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(f"{self.metric_identifier} : Test for commit linkage to issues is not implemented.")
+        return test_status
+
+    def testOtherProvenanceTools(self):
+        """The software project uses other tools to capture detailed machine readable provenance information.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testOtherProvenanceTools"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(
+                f"{self.metric_identifier} : Test for other tools capturing provenance information is not implemented."
+            )
+        return test_status
+
+    def testIssueLinkedToBranch(self):
+        """Code that addresses an issue is developed in a branch prefixed with the issue number.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testIssueLinkedToBranch"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(f"{self.metric_identifier} : Test for issue code on branches is not implemented.")
+        return test_status
+
+    def testPRLinkedToIssue(self):
+        """Links to Pull Requests are included in issue tracker tickets.
+
+        Returns:
+            bool: True if the test was defined and passed. False otherwise.
+        """
+        agnostic_test_name = "testPRLinkedToIssue"
+        test_status = False
+        test_defined = False
+        for test_id in self.metric_test_map[agnostic_test_name]:
+            if self.isTestDefined(test_id):
+                test_defined = True
+                break
+        if test_defined:
+            self.logger.warning(f"{self.metric_identifier} : Test for issues linked to PRs is not implemented.")
         return test_status
 
     def evaluate(self):
@@ -126,6 +318,24 @@ class FAIREvaluatorDataProvenance(FAIREvaluator):
         if self.testProvenanceMetadataAvailable():
             provenance_status = "pass"
         if self.testProvenanceStandardsUsed():
+            provenance_status = "pass"
+        if self.testCitationMetadata():
+            provenance_status = "pass"
+        if self.testProportionalCredit():
+            provenance_status = "pass"
+        if self.testFilesPresent():
+            provenance_status = "pass"
+        if self.testORCIDInZenodoAndCitationFile():
+            provenance_status = "pass"
+        if self.testCommitHistory():
+            provenance_status = "pass"
+        if self.testCommitLinkedToIssue():
+            provenance_status = "pass"
+        if self.testOtherProvenanceTools():
+            provenance_status = "pass"
+        if self.testIssueLinkedToBranch():
+            provenance_status = "pass"
+        if self.testPRLinkedToIssue():
             provenance_status = "pass"
 
         self.result.test_status = provenance_status
