a909f9dcb92576ae9d56a14fcccc751f175ab363
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2024-07-28 18:42:55 +0200

    build: switch to connexion middleware for cors

--- fuji_server/app.py
+++ fuji_server/app.py
@@ -8,7 +8,8 @@ from pathlib import Path
 
 import connexion
 from connexion.jsonifier import Jsonifier
-from flask_cors import CORS
+from connexion.middleware import MiddlewarePosition
+from starlette.middleware.cors import CORSMiddleware
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from fuji_server import encoder
@@ -27,10 +28,19 @@ def create_app(config):
 
     API_YAML = ROOT_DIR.joinpath(YAML_DIR, config["SERVICE"]["openapi_yaml"])
 
+    # Ref: https://connexion.readthedocs.io/en/latest/cookbook.html#cors
+    if os.getenv("ENABLE_CORS", "False").lower() == "true":
+        app.add_middleware(
+            CORSMiddleware,
+            position=MiddlewarePosition.BEFORE_EXCEPTION,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
     app.add_api(API_YAML, validate_responses=True, jsonifier=myjsonifier)
 
     app.app.wsgi_app = ProxyFix(app.app.wsgi_app, x_for=1, x_host=1)
-    if os.getenv("ENABLE_CORS", "False").lower() == "true":
-        CORS(app.app)
 
     return app
