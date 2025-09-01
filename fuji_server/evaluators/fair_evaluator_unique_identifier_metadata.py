ee82ce823f60fb158d93ec376c730c62b917ba2e
huberrob <robert@stratigraphy.net>
2025-09-01 14:35:51 +0200

    minor change

--- fuji_server/evaluators/fair_evaluator_unique_identifier_metadata.py
+++ fuji_server/evaluators/fair_evaluator_unique_identifier_metadata.py
@@ -187,7 +187,7 @@ class FAIREvaluatorUniqueIdentifierMetadata(FAIREvaluator):
             self.output.unique_identifiers = []
             self.result.test_status = "fail"
             self.score.earned = 0
-            print("METRIC VERS : ", type(self.fuji.metric_helper.get_metric_version()))
+            print("METRIC VERS : ", self.fuji.metric_helper.get_metric_version())
             if self.fuji.metric_helper.get_metric_version() <= 0.5:
                 if self.testMetadataIdentifierCompliesWithUUIDorHASH():
                     self.result.test_status = "pass"
