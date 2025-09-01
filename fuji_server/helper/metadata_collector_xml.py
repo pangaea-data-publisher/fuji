073edcd9fc69afcb1838a2a7f2a62af1fc96b6a3
huberrob <robert@stratigraphy.net>
2025-09-01 17:27:48 +0200

    think this fixes #553

--- fuji_server/helper/metadata_collector_xml.py
+++ fuji_server/helper/metadata_collector_xml.py
@@ -252,7 +252,8 @@ class MetaDataCollectorXML(MetaDataCollector):
 
         if xml_metadata:
             if requestHelper.checked_content_hash:
-                requestHelper.checked_content.get(requestHelper.checked_content_hash)["checked"] = True
+                if requestHelper.checked_content.get(requestHelper.checked_content_hash):
+                    requestHelper.checked_content.get(requestHelper.checked_content_hash)["checked"] = True
             self.logger.info("FsF-F2-01M : Found some metadata in XML -: " + (str(xml_metadata.keys())))
         else:
             self.logger.info("FsF-F2-01M : Could not identify metadata properties in XML")
