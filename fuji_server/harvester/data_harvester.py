ad252ae5059319a956aaab7391ff8f6c698fdadd
huberrob <robert@stratigraphy.net>
2026-07-15 20:16:31 +0200

    async; changed user agent to : Mozilla/5.0 (compatible; F-UJI/4.0; +https://github.com/pangaea-data-publisher/fuji)

--- fuji_server/harvester/data_harvester.py
+++ fuji_server/harvester/data_harvester.py
@@ -72,7 +72,8 @@ class DataHarvester:
     LOG_FAILURE = 35
 
     def __init__(self, data_links, logger, landing_page=None, auth_token=None, auth_token_type="Basic", metrics=None):
-        self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; F-UJI)"
+        # self.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; F-UJI)"
+        self.user_agent = "Mozilla/5.0 (compatible; F-UJI/4.0; +https://github.com/pangaea-data-publisher/fuji)"
         self.logger = logger
         self.data_links = data_links
         self.auth_token = auth_token
